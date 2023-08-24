3afc5f09fe15a99626a614bb94cf5b1da347abbc
jayaadityasrikarun <119720478+jayaadityasrikarun@users.noreply.github.com>
2023-08-24 15:56:43 +0530

    a=input() if a=='V':     print('Violet') elif a=='I':     print('Indigo') elif a=='B':     print('Blue') elif a=='G':     print('Green') elif a=='Y':     print('Yellow') elif a=='O':     print('Orange') elif a=='R':     print('Red') else:     print(-1)

--- Print_the_color_name.py
+++ Print_the_color_name.py
@@ -1,18 +1,17 @@
 a=input()
-if a=="V":
-    print("Violet")
-elif a=="I":
-    print("Indigo")
-elif a=="B":
-    print("Blue")
-elif a=="G":
-    print("Green")
-elif a=="Y":
-    print("Yellow")
-elif a=="O":
-    print("Orange")
-elif a=="R":
-    print("Red")
+if a=='V':
+    print('Violet')
+elif a=='I':
+    print('Indigo')
+elif a=='B':
+    print('Blue')
+elif a=='G':
+    print('Green')
+elif a=='Y':
+    print('Yellow')
+elif a=='O':
+    print('Orange')
+elif a=='R':
+    print('Red')
 else:
-    print("-1")
-    
+    print(-1)
